27fbcde9dc65574952eb5b13431367a2919f83f5
Sohan <sohan.masireddy@gmail.com>
2026-08-12 16:54:22 -0400

    1

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ with controls:
 with editor:
     st.text_area(
         "Your note",
-        height=332,
+        height=331,
         placeholder="Start writing here...",
         key="note",
         label_visibility="collapsed",
